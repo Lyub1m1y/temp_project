a6b132c108e359a69c02999c0bfe1e6c998e0a97
root <root@DESKTOP-2213A4U.localdomain>
2022-12-07 00:24:17 +0400

    add task_4 done

--- sanya/task_4.cpp
+++ sanya/task_4.cpp
@@ -1,31 +1,12 @@
-#include <cstring>
 #include <iostream>
-
+#include <string>
 using namespace std;
-
-int counterWord(int n, string strInput) {
-  int sum = 0;
-  int length = strInput.size();
-  for (int i = 0, j = 0; i < length; i++) {md
-    if (strInput.at(i) != ' ') {
-      if (j < n && strInput.at(j + 1) == (' ' || strInput.at(length))) {
-        sum++;
-        j++;
-      } else {
-        j = 0;
-      }
-    }
-  }
-  return sum;
-}
-
 int main() {
-  int n = 0;
-  string strInput;
-  cin >> n;
-  cin >> strInput;
-  int sum = 0;
-  sum = (counterWord(n, strInput));
-  cout << sum << endl;
+  int p;
+  string a, word;
+  cin >> word;
+  cin >> a;
+  p = a.find(word);
+  cout << p << endl;
   return 0;
 }
